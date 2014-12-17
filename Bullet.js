e438917932a35526c4e7279a8639775a470c4ae3
Obodrel <reynald.pereira@gmail.com>
2014-12-17 14:06:48 +0100

    levelScreen ref chagned into game.currentScreen

--- Bullet.js
+++ Bullet.js
@@ -26,13 +26,13 @@ Bullet.prototype = {
 			this.x+=level*this.xSpeed;
 			this.y+=level*this.ySpeed;
 		}
-		if(this.hitTest(levelScreen.player) && this.angle == 0) {
+		if(this.hitTest(game.currentScreen.player) && this.angle == 0) {
 			game.numLifes--;
 			delete this;
 		}
-		for(i = 0; i<levelScreen.enemies.length;++i) {
-			if(levelScreen.enemies[i] != null && this.hitTest(enemies[i])) {
-				delete levelScreen.enemies[i];
+		for(i = 0; i<game.currentScreen.enemies.length;++i) {
+			if(game.currentScreen.enemies[i] != null && this.hitTest(enemies[i])) {
+				delete game.currentScreen.enemies[i];
 				delete this;				
 			}	
 		}
--- BulletFactory.js
+++ BulletFactory.js
@@ -4,14 +4,14 @@ function BulletFactory(){
 BulletFactory.prototype = {
 	createBullet(weaponId) {
 		if (weaponId == 1) {
-			return (new Bullet(levelScreen.player.x+1,player.y,0,["AllyBullet"],["Enemy"]));
+			return (new Bullet(game.currentScreen.player.x+1,player.y,0,["AllyBullet"],["Enemy"]));
 		}
 		if (weaponId == 2) {
 			bullet[] = new Bullet[2];
-			bullet[0] = new Bullet(levelScreen.player.x+1,player.y+1,-45,["AllyBullet"],["Enemy"]);
-			bullet[0] = new Bullet(levelScreen.player.x+1,player.y-1,45,["AllyBullet"],["Enemy"]);
+			bullet[0] = new Bullet(game.currentScreen.player.x+1,player.y+1,-45,["AllyBullet"],["Enemy"]);
+			bullet[0] = new Bullet(game.currentScreen.player.x+1,player.y-1,45,["AllyBullet"],["Enemy"]);
 			return bullet;
 		}	
-		return (new Bullet(weaponId/10,weaponId%10,["EnemyBullet"],["Player"]));
+		return (new Bullet(weaponId/10,weaponId%10,180,["EnemyBullet"],["Player"]));
 	}
 }
