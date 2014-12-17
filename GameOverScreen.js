8a30a59d7415c79f73a078d72eef3c4de873d425
Victor Matteucci <victor.matteucci@ecole.ensicaen.fr>
2014-12-17 10:01:22 +0100

    modif gos et index

---
+++ GameOverScreen.js
@@ -0,0 +1,28 @@
+var GameOverScreen=Class.create(GameScreen,{
+
+
+initialize:function(){
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext('2d');
+  render(ctx);
+},
+
+
+update:function(){
+
+},
+
+render:function(graphics){
+graphics.fillRect(0,0, window.screen.availWidth,window.screen.availHeight);
+graphics.font = "18px Helvetica";
+graphics.fillText("Game Over", 0,30);
+},
+
+dispose:function(){
+
+}
+
+});
+
+var gameoverscreen=new GameOverScreen();
+
