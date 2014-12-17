0e93b90f25dd940b0115c283fb8f2f3af6c1b401
Obodrel <reynald.pereira@gmail.com>
2014-12-17 13:31:58 +0100

    LevelScreen&Animation missing, rest done, except GameEntity and InfiniteBackground that Thomas is in charge of

---
+++ IShooter.js
@@ -0,0 +1,9 @@
+function IShooter(){
+	
+}
+
+IShooter.prototype = {
+	shoot : function() {
+	
+	}
+}
--- Player.js
+++ Player.js
@@ -1,5 +1,5 @@
-function Player(x,y){
-	GameEntity.call(this,x,y);
+function Player(x,y,collisionGroups,collisionFilters){
+	GameEntity.call(this,x,y,collisionGroups,collisionFilters);
 	IShooter.call(this);
 }
 
@@ -7,39 +7,45 @@ Player.prototype = {
 	this.xSpeed = 5;
 	this.ySpedd = 5;
 	this.weaponId = input.WEAPON_1;
+	this.isSecondWeaponAvailable = false;
+	this.hitBox = new Rectangle(x-2,y-2,x+2,y+2);
 	shoot : function() {
 		if( input.isKeyDown(input.SHOOT)) {
-			bulletFactory.createBullet(this.weaponId);
-		}
-	},
-	canCollideWith : function( entity ) {
-		switch (Object.getPrototypeOf(entity)) {
-			case Enemy.prototype :
-				return true;
-			case Bullet.prototype :
-				return true;
-			case Bonus.prototype :
-				return true;
-			default :
-				return false;
+			if (this.weaponId == 1 ) {
+				levelScreen.addEntity(bulletFactory.createBullet(this.weaponId));
+			} else {
+				for(e in bulletFactory.createBullet(this.weaponId)) {
+					levelScreen.addEntity(e);
+				}
+			}
 		}
 	},
 	update : function() {
 		if( input.isKeyDown(input.UP)) { 
 			this.hitBox.moveTo(this.x,this.y-level*this.ySpeed);
+			this.y-=level*this.ySpeed:
 		}
 		if( input.isKeyDown(input.LEFT)) { 
 			this.hitBox.moveTo(this.x-level*this.xSpeed,this.y);
+			this.x-=level*this.xSpeed;
 		}
 		if( input.isKeyDown(inputDOWN)) { 
 			this.hitBox.moveTo(this.x,this.y+level*this.ySpeed);
+			this.x-=level*this.xSpeed;
 		}
 		if( input.isKeyDown(input.RIGHT)) { 
 			this.hitBox.moveTo(this.x+level*this.xSpeed,this.y);
+			this.x+=level*this.xSpeed;
 		}
+		if( input.isKeyDown(input.WEAPON_1)) { 
+			weaponId = 1;
+		}		
+		if( this.isSecondWeaponAvailable && input.isKeyDown(input.WEAPON_2)) { 
+			weaponId = 2;
+		}		
 	},
 	render : function( graphics ) {
-		graphics.drawImage("./assests/image/Spaceship01.png",this.x,this.y);
+		graphics.drawImage("player1",this.x,this.y);
 	},
 	hitTest : function( entity ) {
 		if(this.canCollideWith(entity) && entity.hitBox.intersect(this.hitBox)) {
