4b37d5b8ad05869fb938c64dd3d2e962b1fb48a9
Obodrel <reynald.pereira@gmail.com>
2014-12-17 10:03:10 +0100

    Bonus finished

---
+++ Bonus.js
@@ -0,0 +1,39 @@
+function Bonus(){
+	GameEntity.call(this,x,y);
+}
+
+Bonus.prototype = {
+	this.xSpeed = 1;
+	this.ySpedd = 0;
+	canCollideWith : function( entity ) {
+		switch (Object.getPrototypeOf(entity)) {
+			case Player.prototype :
+				return true;
+			default :
+				return false;
+		}
+	}
+	update : function() {
+		level = 0;
+		score = game.score;
+		while (score > 100) {
+			score -= 100;
+			level++;
+		}
+		this.hitBox.moveTo(this.x-level*this.xSpeed,this.y-level*this.ySpeed);
+	}
+	render : function( graphics ) {
+		graphics.drawImage("./assests/image/Enemy02.png",this.x,this.y);
+	}
+	hitTest : function( entity ) {
+		if(this.canCollideWith(entity) && entity.hitBox.intersect(this.hitBox)) {
+			return true;
+		}
+		return false;
+	}
+};
+Bonus.prototype = $.extend(
+	{},
+	GameEntity.prototype,
+	Bonus.prototype
+);
