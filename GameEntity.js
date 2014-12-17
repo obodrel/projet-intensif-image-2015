bcfd95180293ef0924b0739ac552b703d18177f6
thomas <guibert.th@gmail.com>
2014-12-17 09:42:47 +0100

    Modification GameEntity

--- GameEntity.js
+++ GameEntity.js
@@ -1,9 +1,9 @@
 var GameEntity = Class.create({
-	initialize: function(params){
+	initialize : function(params){
 		this.x=params.x;
 		this.y=params.y;
 		this.z=params.z;
 		this.collisionGroup= params.collisionGroup;
 		this.collisionFilters= params.collisionFilters;
 	}
-}
+});
