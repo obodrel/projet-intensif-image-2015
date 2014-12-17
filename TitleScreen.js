ae1e75069b9a19e62ddbff2c6cd5386901432338
Chamoux <thibaut.chamoux@ecole.ensicaen.fr>
2014-12-17 11:44:19 +0100

    singleton input + debut titlescreen

---
+++ TitleScreen.js
@@ -0,0 +1,36 @@
+var TitleScreen = Class.create(GameScreen, {
+	
+	init: function() {
+		
+		
+	},
+
+	update: function() {
+		
+	},
+
+	dispose: function() {
+	
+	},
+
+	render: function(ctx) {
+
+		canvas = context.canvas;
+		height = canvas.height;
+		width = canvas.width;				
+		ctx.strokeStyle="#FF0000";
+		ctx.strokeRect(0,0,width,height); 
+		ctx.font = Math.floor(width/10)+"Calibri,Geneva,Arial";
+    		ctx.strokeStyle = "rgb(0,0,0)";
+		var txt = "Press space bar to play";
+    		ctx.fillText(txt, width/3, height/2);
+
+		
+	}
+
+});
+
+var titlescreen = new TitleScreen();
+var canvas = document.getElementById('canvas');
+context = canvas.getContext('2d');
+titlescreen.render(context);
