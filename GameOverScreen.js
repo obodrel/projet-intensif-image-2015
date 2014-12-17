f47a757986244c424f9c8a4a6d3f09351f6d70c4
Victor Matteucci <victor.matteucci@ecole.ensicaen.fr>
2014-12-17 10:14:35 +0100

    GOverS edit

--- GameOverScreen.js
+++ GameOverScreen.js
@@ -1,19 +1,15 @@
 var GameOverScreen=Class.create(GameScreen,{
 
 
-initialize:function(){
-  canvas = document.getElementById("canvas");
-  ctx = canvas.getContext('2d');
-  
-},
-
 
 update:function(){
 
 },
 
 render:function(graphics){
+graphics.fillStyle = "#ff0000"; //Toutes les prochaines formes pleines seront rouges.
 graphics.fillRect(0,0, window.screen.availWidth,window.screen.availHeight);
+
 graphics.font = "18px Helvetica";
 graphics.fillText("Game Over", 0,30);
 },
@@ -25,4 +21,6 @@ dispose:function(){
 });
 
 var gameoverscreen=new GameOverScreen();
+canvas = document.getElementById("canvas");
+ctx = canvas.getContext('2d');
 gameoverscreen.render(ctx);
