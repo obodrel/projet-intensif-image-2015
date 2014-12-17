85f4c77fbe83d3633636729d60f1efaa23fd4abe
Victor Matteucci <victor.matteucci@ecole.ensicaen.fr>
2014-12-17 15:24:18 +0100

    màj de Audio.js

--- Audio.js
+++ Audio.js
@@ -1,3 +1,23 @@
 var Audio = Class.create({
 
+function playBGM(id){
+
+if(game.score<=100){
+ var audio=assetsManager.getAudio("lvl1")
+ audio.play();
+}
+
+else {
+ var audio=assetManager.getAudio("lvl2");
+ audio.play();
+}
+}
+
+function playFX(id){
+
+}
+
+
 });
+
+var  audio = new Audio();
